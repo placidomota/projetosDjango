b6b2910938a7f8b01194199fd8b827747abaace5
placido <placidobb.ac@gmail.com>
2023-05-16 19:07:20 -0300

    Adicionando arquivos estaticos

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def index(request):
-    return HttpResponse('<h1>Alura space</h1><p>Ola alura</p>')
+    return render(request, 'galeria/index.html')
 
