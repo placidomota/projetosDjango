72b232a348df21f126161af0dbe2dca7b6af541f
placido <placidobb.ac@gmail.com>
2023-05-20 19:42:53 -0300

    Integrando o django a um banco de dados e configurando o admin

--- setup/settings.py
+++ setup/settings.py
@@ -129,6 +129,13 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+#Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
